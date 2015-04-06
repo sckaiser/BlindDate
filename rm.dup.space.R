3ff46cc61e48e547980e433b61a3f325cdae6059
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-06 13:20:39 -0700

    more low-level utilities

---
+++ rm.dup.space.R
@@ -0,0 +1,5 @@
+rm.dup.space <- function(x) {
+  # Removes duplicate spaces using a lookbehind approach.
+  # Ref: https://stackoverflow.com/questions/14737266/removing-multiple-spaces-and-trailing-spaces-using-gsub
+  return(gsub("^ *|(?<= ) | *$", "", x, perl=T))
+}
