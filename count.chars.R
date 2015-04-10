3355fca7a1e7f4f31bf81665a551a5cfbffc4cb3
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-10 08:36:47 -0700

    removed files with old names

--- count.chars.R
+++
@@ -1,9 +0,0 @@
-count.chars <- function(x, y) {
-  # Counts the number of times a given character is found
-  # Args:
-  #  x, a character vector
-  #  y, a character of length one.
-  n.spaces <- gregexpr(y, x, fixed = T)  # get locations of spaces
-  n.spaces <- lapply(n.spaces, length)  # count number of spaces
-  unlist(n.spaces)  # convert to vector
-}
--- true.mode.R
+++
@@ -1,6 +0,0 @@
-true.mode <- function(x) {
-  # Calculates the most frequent value in an vector.
-  # Ref: https://stackoverflow.com/questions/2547402/standard-library-function-in-r-for-finding-the-mode
-  ux <- unique(x)
-  ux[which.max(tabulate(match(x, ux)))]
-}
