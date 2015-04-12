72b8180116f191707b445c2b30a1300eb62847ec
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-11 18:04:55 -0700

    corrected bugs in the comments

--- CountChars.R
+++ CountChars.R
@@ -3,7 +3,7 @@ CountChars <- function(x, y) {
   # Args:
   #  x, a character vector,
   #  y, a character of length one.
-  n.spaces <- gregexpr(y, x, fixed = T)  # get locations of spaces
-  n.spaces <- lapply(n.spaces, length)  # count number of spaces
+  n.spaces <- gregexpr(y, x, fixed = T)  # get locations of the character
+  n.spaces <- lapply(n.spaces, length)  # count number of occurances
   unlist(n.spaces)  # convert to vector
 }
