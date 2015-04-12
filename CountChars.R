76fdd6958c8e03ba3ad84b7b8c15bc860d7536e0
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-11 18:11:19 -0700

    added some error handling

--- CountChars.R
+++ CountChars.R
@@ -3,6 +3,11 @@ CountChars <- function(x, y) {
   # Args:
   #  x, a character vector,
   #  y, a character of length one.
+  len.y <- length(y)
+  if (len.y != 1) {
+    print(paste("The second argument was not of length 1. It was of length:", len.y))
+    stop()
+  }
   n.spaces <- gregexpr(y, x, fixed = T)  # get locations of the character
   n.spaces <- lapply(n.spaces, length)  # count number of occurances
   unlist(n.spaces)  # convert to vector
