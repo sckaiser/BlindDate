8f5be4d5cf221d1ef374bb2081ce218d3f2de6ec
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-12 19:32:32 -0700

    changed variable names to match intent

--- CountChars.R
+++ CountChars.R
@@ -5,10 +5,11 @@ CountChars <- function(x, y) {
   #  y, a character of length one.
   len.y <- length(y)
   if (len.y != 1) {
+    
     print(paste("The second argument was not of length 1. It was of length:", len.y))
     stop()
-  }
-  n.spaces <- gregexpr(y, x, fixed = T)  # get locations of the character
-  n.spaces <- lapply(n.spaces, length)  # count number of occurances
-  unlist(n.spaces)  # convert to vector
+  } # need to add error checking for nchar(y) != 1
+  which.char.y <- gregexpr(y, x, fixed = T)  # get locations of the character
+  n.char.y <- lapply(which.char.y, length)  # count number of occurances
+  unlist(n.char.y)  # convert to vector
 }
