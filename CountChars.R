79caf378a19b33439114e640fc46e65e7b3ff87b
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-13 11:54:15 -0700

    Fixed bug when character isn't found

--- CountChars.R
+++ CountChars.R
@@ -1,20 +1,33 @@
 CountChars <- function(x, y) {
-  # Counts the number of times a given character is found
+  # Counts how often a single character appears in another character vector.
   # Args:
   #  x, a character vector,
   #  y, a length one character vector containing a single character.
-  len.y <- length(y)
-  if (len.y != 1) {
-    print(paste("Errorr: The second argument must be length 1. It was length:", len.y))
+  # Returns:
+  #  n.char.y, the number of times y is found in each string in x.
+  
+  len.y  <- length(y)
+  nchars <- nchar(y)
+  if (len.y  != 1) {
+    print("Error: The second argument must be length 1.")
+    print(paste("It was length:", len.y))
     stop()
   }
-  if (nchar(y[1]) != 1) {
+  if (nchars != 1) {
     print("Error: The second argument must have exactly one character.")
+    print(paste("The argument provded had", nchars, "characters:", y))
     stop()
-  }  
-  which.char.y <- gregexpr(y, x, fixed = T)  # get locations of the character
-  n.char.y <- lapply(which.char.y, length)  # count number of occurances
-  unlist(n.char.y)  # convert to vector
+  }
+  
+  x.char.y  <- gregexpr(y, x, fixed = T)  # get y's locations in x
+  n.char.y  <- lapply(x.char.y, length)  # count y's occurances
+  n.char.y  <- unlist(n.char.y)  # convert to vector
+  
+  # Handle when gregexpr returns -1 (i.e., not found)
+  first     <- function(z) { z[1] }  # helper function
+  not.found <- lapply(x.char.y, first)  # get the first match
+  not.found <- unlist(not.found)   # convert to vector
+  not.found <- which(not.found == -1)  # -1 indicates no match
+  n.char.y[not.found] <- 0  # set non-matches to zero.
+  return(n.char.y)
 }
-
-# 2. Doesn't handle when gregexpr returns -1 (i.e., not found)
