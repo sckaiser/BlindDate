024ead1832b91d15ba747ed395af3fcb479d005d
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-16 08:43:34 -0700

    function to reduce WET code in GuessFormat

---
+++ YearLength.R
@@ -0,0 +1,12 @@
+YearLength <- function(date.pos) {
+  # Given an integer vector of years, returns y for 2 digit years;
+  # otherwise, returns Y (implying 3 or more digit years).
+  year.digits <- nchar(date.pos) # count number of digits
+  year.digits <- TrueMode(year.digits) # pick most common.
+  if (year.digits == 2) {
+    date.format <- "y" 
+  } else {
+    date.format <- "Y"
+  }
+  return(date.format)
+}
