14e221b17f0f41992905993befdb2a115d1e9452
Scott Kaiser <scott.kaiser@gmail.com>
2015-04-18 07:38:24 -0700

    style cleanup

--- RmDupSpace.R
+++ RmDupSpace.R
@@ -1,5 +1,5 @@
 RmDupSpace <- function(x) {
   # Removes duplicate spaces using a lookbehind approach.
   # Ref: https://stackoverflow.com/questions/14737266/removing-multiple-spaces-and-trailing-spaces-using-gsub
-  return(gsub("^ *|(?<= ) | *$", "", x, perl=T))
+  gsub("^ *|(?<= ) | *$", "", x, perl = T)
 }
