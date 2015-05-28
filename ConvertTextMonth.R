3e96cd9b41aeef8d17e4a1fa0cfdd4c72fc5e1cf
Scott Kaiser <scott.kaiser@gmail.com>
2015-05-28 09:51:29 -0700

    moved TODO to github issue

--- ConvertTextMonth.R
+++ ConvertTextMonth.R
@@ -7,7 +7,7 @@ ConvertTextMonth <- function(x, convert = T, ignore.null = T) {
   # Returns:
   #  if convert = T, a character vector with text months replaced by numbers.
   #  if convert = F, a numeric indicating the proportion of x with text months.
-  # TODO: Handle ties.
+  
   m1         <- c("January", "February", "March", "April",
                   "May", "June", "July", "August",
                   "September", "October", "November", "December")
