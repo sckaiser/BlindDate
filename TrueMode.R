d1754346f49339fc08b64403e50e1ce63e8429da
Scott Kaiser <scott.kaiser@gmail.com>
2015-06-08 08:46:41 -0700

    explicit NA handling

--- TrueMode.R
+++ TrueMode.R
@@ -1,6 +1,12 @@
-TrueMode <- function(x) {
+TrueMode <- function(x, na.rm = F) {
   # Calculates the most frequent value in an vector.
   # Ref: https://stackoverflow.com/questions/2547402/standard-library-function-in-r-for-finding-the-mode
+  # Args:
+  #  x, a vector
+  #  na.rm, whether to remove NA values.
+  if (na.rm) {
+    x <- x[!is.na(x)]
+  }
   ux <- unique(x)
   ux[WhichMax(tabulate(match(x, ux)))]
 }
