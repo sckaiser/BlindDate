5daaeac5a6cf01ee201111681fd34ee04c18ca5c
Scott Kaiser <scott.kaiser@gmail.com>
2015-06-30 07:59:58 -0700

    source new TokenizeDt() function

--- ConvertDateTime_test.R
+++ ConvertDateTime_test.R
@@ -4,8 +4,8 @@ library(stringr)
 setwd("C:/Users/scott_kaiser/Desktop/Analytics and Data Science/BlindDate")
 files <- c("ConvertDateTime.R", "ConvertTextMonth.R", "CountChars.R",
            "CountElements.R", "GreplAny.R", "GuessFormat.R", "MultiGsub.R",
-           "RmDupSpace.R", "SetPartition.R", "TrueMode.R", "UniqueOrder.R",
-           "WhichMax.R", "YearLength.R")
+           "RmDupSpace.R", "SetPartition.R", "TokenizeDt.R", "TrueMode.R",
+           "UniqueOrder.R", "WhichMax.R", "YearLength.R")
 sapply(files, source)
 
 # start at midnight, January 1. Add 61 minutes successively
