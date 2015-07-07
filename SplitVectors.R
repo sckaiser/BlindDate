cb624bdbd4404a146193fd06f5dbb9264a74190f
Scott Kaiser <scott.kaiser@gmail.com>
2015-07-07 09:49:59 -0700

    initial commit

---
+++ SplitVectors.R
@@ -0,0 +1,21 @@
+SplitVectors <- function(x, n) {
+  # Splits a vector, or a list of vectors, into separate vectors. This can be
+  # used in processing dates to convert, e.g. a d1-m1-y1, d2-m2-y2, ... 
+  # structure into a d1, d2, ... a m1, m2, ... and a y1, y2, ... structure.
+  # Args:
+  #  x, a vector or a list of vectors.
+  #  n, the number of new vectors into which x should be split.
+  # Returns:
+  #  a list of n vectors.
+  seq1 <- seq(1, length(x) + 1 - n, by = n)  # baseline sequence for splitting
+  # Next: Create subsequent sequences
+}
+
+x <- c(12, 1, 2015, 11, 29, 2015)
+n <- 3
+
+x <- c(12, 2015, 11, 2015)
+n <- 2
+
+x <- c(2015, 2014)
+n <- 1
