bb3689da0b5a1d6535bc0b368478bfac561b0b00
Scott Kaiser <scott.kaiser@gmail.com>
2015-07-08 11:17:34 -0700

    perform the split using lapply

--- SplitVectors.R
+++ SplitVectors.R
@@ -6,29 +6,20 @@ SplitVectors <- function(x, n) {
   #  x, a vector or a list of vectors.
   #  n, the intger number of new vectors into which x should be split.
   # Returns:
-  #  a list of n vectors.
+  #  a length-n list of vectors.
+  
+  IdxHelper <- function(idx, x) x[idx]  # subsets a vector x using idx
   
   if (n > 1) {
     seq1        <- seq(1, length(x) + 1 - n, by = n)  # first splitting sequence 
-    seq.list   <- list(seq1)
+    seq.list    <- list(seq1)
     for (i in 2:n) {
-      seqx      <- seq.list[[i - 1]] + 1  # create subsequent sequences
-      seq.list <- c(seq.list, list(seqx))
-      # TODO: actually split the vector
+      seqx      <- seq.list[[i - 1]] + 1  # create next sequence
+      seq.list  <- c(seq.list, list(seqx))  # append it
+      vect.list <- lapply(seq.list, IdxHelper, x)
     }
   } else {
     vect.list   <- list(x)  # handle 'don't split' case
   }
   return(vect.list)
 }
-
-IdxHelper <- function(idx, x) x[idx]  # subsets a vector x using idx
-
-x <- c(12, 1, 2015, 11, 29, 2015, 10, 31, 2015)
-n <- 3
-
-x <- c(12, 2015, 11, 2015, 10, 2015)
-n <- 2
-
-x <- c(2015, 2014, 2016)
-n <- 1
