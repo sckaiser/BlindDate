e12586df52604bf3a73a613494b3af09d56cbbb3
Scott Kaiser <scott.kaiser@gmail.com>
2015-07-19 19:18:33 -0400

    rename vector t to tm to avoid confusing w/ t()

--- ConvertDateTime_test.R
+++ ConvertDateTime_test.R
@@ -1,4 +1,4 @@
-# Format test harness for Convert Format script.
+# Test harness for ConvertDateTime.R script.
 library(lubridate)
 library(stringr)
 setwd("C:/Users/scott_kaiser/Desktop/Analytics and Data Science/BlindDate")
@@ -8,19 +8,19 @@ SourceDir(exclude = c("format utility.R", "ConvertDateTime_test.R"))
 # start at midnight, January 1. Add 61 minutes successively
 t.size     <- 24 * 365
 start.date <- as.POSIXct("01-01-2015 00:00:00", format = "%m-%d-%Y %H:%M:%S")
-t          <- rep(start.date, t.size)
+tm         <- rep(start.date, t.size)
 increment  <- 61 * 60  # increment just more than an hour to generate more times
 for (i in 2:t.size) {
-  t[i] <- t[i - 1] + increment
+  tm[i] <- tm[i - 1] + increment
 }
 
 # One-time initialization:
-# write.table(t, "TestDates.csv", sep = ",", col.names = F, row.names = F)
+# write.table(tm, "TestDates.csv", sep = ",", col.names = F, row.names = F)
 # open in Excel; change formats. Lots of formats.
 
 test.mat    <- read.csv("TestDates.csv", header = F, stringsAsFactors = F)
 convert.mat <- matrix(NA, nrow = nrow(test.mat), ncol = ncol(test.mat))
-convert.mat           <- data.frame(convert.mat)
+convert.mat <- data.frame(convert.mat)
 colnames(convert.mat) <- colnames(test.mat)
 
 for (i in 1:ncol(test.mat)) {
@@ -33,7 +33,7 @@ for (i in 1:ncol(test.mat)) {
 # This following section is loop-free,
 # but harder to debug when a single format conversion fails.
 # convert.mat <- apply(test.mat, 2, ConvertDateTime)
-results.mat <- apply(convert.mat, 2, identical, t)
+results.mat <- apply(convert.mat, 2, identical, tm)
 results     <- apply(results.mat, 2, all)
 
 # 
