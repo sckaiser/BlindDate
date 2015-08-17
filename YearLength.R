f5b82a6c18844b439584997af5b77dab3213e6c6
sckaiser <scott.kaiser@gmail.com>
2015-08-17 08:21:24 -0700

    use stopifnot()

--- YearLength.R
+++ YearLength.R
@@ -1,11 +1,10 @@
 YearLength <- function(date.pos) {
   # Given an integer vector of years, returns y for 2 digit years;
   # otherwise, returns Y (implying 3 or more digit years).
+  
   year.digits <- nchar(date.pos) # count number of digits
   year.digits <- TrueMode(year.digits) # pick most common.
-  if (year.digits < 2) {
-    stop("The year argument had less than two digits")
-  }
+  stopifnot (year.digits >= 2)
   if (year.digits == 2) {
     date.format <- "y" 
   } else {
