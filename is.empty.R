127859650398f11a7a9755c44a9fd20a612b0ec5
sckaiser@github.com <scott.kaiser@gmail.com>
2015-09-08 08:58:37 -0700

    create empty vector of same class with x[0]

--- is.empty.R
+++ is.empty.R
@@ -6,7 +6,5 @@ is.empty <- function(x, ignore.nm = T) {
   if (ignore.nm) {
     x <- as.vector(x)  # remove any attributes
   }
-  identical(x, logical(0)) | identical(x, integer(0))   |
-  identical(x, numeric(0)) | identical(x, character(0)) |
-  identical(x, complex(0)) | identical(x, raw(0))        
+  identical(x, x[0]) 
 }
