23b00d432f9468b389346f33e461e8a9f2fca7a6
sckaiser@github.com <scott.kaiser@gmail.com>
2015-09-15 11:13:53 -0700

    new function to increase DRY code in root functions

---
+++ CleanText.R
@@ -0,0 +1,11 @@
+CleanText <- function(x) {
+  # Removes extraneous spaces and replaces text variants of NA with NA
+  # Args:
+  #  x, a character string
+  
+  stopifnot(is.character(x))
+  x                <- RmDupSpace(x) # trim leading, trailing & duplicate spaces
+  NA.syn           <- c("", "NULL", "Not Applicable", "NA", "N/A")
+  x[x %in% NA.syn] <- NA  # convert NA synonyms
+  return(x)
+}
