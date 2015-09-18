0f897a060bfaaf39a04df55a908ccf5ce76df4d4
sckaiser@github.com <scott.kaiser@gmail.com>
2015-09-18 10:17:56 -0700

    spacing for readability

--- is.datechar.R
+++ is.datechar.R
@@ -6,21 +6,21 @@ is.datechar <- function(x) {
   #  a length-one logical indicating whether the vector is "date-as-character".
 
   if (is.character(x)) {
-    x            <- CleanText(x)  # handle extra spaces and NA-synonyms
-    x            <- sub("\\.[0-9]+$", "", x)  # remove trailing decimals
-    sample.size  <- 4000
-    sample.size  <- min(sample.size, length(x))
-    x            <- x[sample(length(x), sample.size)]  # sample for speed
+    x           <- CleanText(x)  # handle extra spaces and NA-synonyms
+    x           <- sub("\\.[0-9]+$", "", x)  # remove trailing decimals
+    sample.sz   <- 4000
+    sample.sz   <- min(sample.sz, length(x))
+    x           <- x[sample(length(x), sample.sz)]  # sample for speed
     
-    text.month   <- ConvertTextMonth(x, F)
+    text.month  <- ConvertTextMonth(x, F)
     if (text.month > .5) {  # if more than 50% have text months...
       T  # return True
     } else {
-      x          <- MultiGsub(c("AM", "PM"), c("", ""), x)  # Remove AM & PM
-      dt.format  <- GuessFormat(x)  # Guess format
+      x         <- MultiGsub(c("AM", "PM"), c("", ""), x)  # Remove AM & PM
+      dt.format <- GuessFormat(x)  # Guess format
       !grepl("-1", dt.format)  # if dt.format has -1, we couldn't fully parse
     }
   } else {
-    F  # if it's not a character vector, it's not date-as-character.
+    F  # if it's not a character vector, it's not date-as-character
   }
 }
