3090f7d3901a7f3144b215ad5957fd17482f3e36
sckaiser@github.com <scott.kaiser@gmail.com>
2015-11-01 08:51:06 -0800

    first draft

---
+++ UnSample.R
@@ -0,0 +1,26 @@
+UnSample <- function(x, sample.sz,
+                     end.sz = max(10, 0.005 * length(sample.sz))) {
+  # Randomly samples most of a vector, but includes beginning and ending values.
+  # This is useful when loading data where unusual values are found at the ends.
+  # For example, for data logged sequentially, early and later values may differ
+  # due to changes in measurement granularity or format at some point during the
+  # life of the logging process.
+  # Args:
+  #  x, a vector.
+  #  sample.sz, the number of samples to return, including those at the ends.
+  #  end.sz, the number of samples from the beginning and end (combined).
+  
+  end.sz    <- round(end.sz)
+  end.sz    <- end.sz + end.sz %% 2
+  stopifnot(length(x) >= sample.sz, sample.sz > end.sz)
+  if (sample.sz >= 15 && end.sz > 0) {
+    end.sz  <- end.sz / 2
+    rand.sz <- sample.sz - end.sz
+    ends    <- c(x[1:end.sz], x[(length(x) - end.sz):length(x)])
+    middle  <- x[end.sz:(length(x) - end.sz)]
+    c(ends, middle[sample(length(middle), rand.sz)])
+  } else {
+    warning("The vector was simply sampled at random.")
+    x[sample(length(x), sample.sz)]
+  }
+}
