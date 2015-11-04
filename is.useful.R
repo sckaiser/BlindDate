54e48f0aa6e11a14dbfbe4cd0a1e6401c0f44c19
sckaiser@github.com <scott.kaiser@gmail.com>
2015-11-04 08:42:17 -0800

    replace sample() with UnSample()

--- is.useful.R
+++ is.useful.R
@@ -12,7 +12,7 @@ is.useful <- function(FUN, x, thr = 1, sample.sz = min(5000, length(x)), ...) {
   #  a logical indicating whether or not FUN changes x.
   
   stopifnot(thr >= 0 && thr <= 1 && sample.sz <= length(x))
-  x     <- x[sample(length(x), sample.sz)]
+  x     <- UnSample(x, sample.sz)
   x     <- x[!is.na(x)]
   match <- length(which(x == FUN(x, ...))) / length(x)
   (match < thr)
