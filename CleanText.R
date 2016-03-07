0750b15bb15fa05ef4049cd3744e453c2fd55bd5
sckaiser@github.com <scott.kaiser@gmail.com>
2016-03-07 12:57:05 -0800

    make NA synonyms an argument + default

--- CleanText.R
+++ CleanText.R
@@ -1,10 +1,9 @@
-CleanText <- function(x) {
+CleanText <- function(x, NA.syn = c("", "NULL", "Not Applicable", "NA", "N/A")) {
   # Removes extraneous spaces and replaces text variants of NA with NA
   # Args:
   #  x, a character string
   
   x                <- RmDupSpace(x) # trim leading, trailing & duplicate spaces
-  NA.syn           <- c("", "NULL", "Not Applicable", "NA", "N/A")
   x[x %in% NA.syn] <- NA  # convert NA synonyms
   x
 }
