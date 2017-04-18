a161a7235469807067c8dcaf3d842d0e835def96
Kaiser <scottkai@amazon.com>
2017-04-18 07:57:22 -0700

    refactor to correctly handle length-1 inputs

--- UnSample.R
+++ UnSample.R
@@ -1,4 +1,4 @@
-UnSample <- function(x, sample.sz, end.sz = max(10, 0.005 * sample.sz)) {
+UnSample <- function(x, sample.sz, end.sz = -1L) {
   # Randomly samples most of a vector, but includes beginning and ending values.
   # This is useful when loading data where unusual values are found at the ends.
   # For example, for data logged sequentially, early and later values may differ
@@ -11,18 +11,31 @@ UnSample <- function(x, sample.sz, end.sz = max(10, 0.005 * sample.sz)) {
   #  end.sz, a length-one integer; the number of samples from the beginning and
   #          from the end (combined).
   
-  end.sz    <- as.integer(round(end.sz))
-  end.sz    <- end.sz + end.sz %% 2L
   stopifnot(length(sample.sz) == 1, length(end.sz) == 1, length(x) >= sample.sz,
             sample.sz > end.sz)
-  if (sample.sz >= 15 && end.sz > 0) {
+  if (end.sz == -1L) {  # set the defaut
+    if (sample.sz <= 3L) {
+      end.sz <- 0L
+    } else if (sample.sz <= 10L) {
+      end.sz <- min(2L, sample.sz - 1L)
+    } else {
+      end.sz <- max(10, 0.005 * sample.sz)
+      end.sz <- as.integer(round(end.sz))
+    }
+    end.sz <- end.sz + end.sz %% 2L
+  }
     end.sz  <- end.sz / 2
     ends    <- c(x[1:end.sz], x[(length(x) - end.sz):length(x)])
     middle  <- x[end.sz:(length(x) - end.sz)]
     rand.sz <- min(sample.sz - end.sz, length(middle))
     c(ends, middle[sample(length(middle), rand.sz)])
-  } else {
-    warning("The vector was simply sampled at random.")
-    x[sample(length(x), sample.sz)]
-  }
 }
+
+
+UnSample(1, 1)
+UnSample(1:100, 1)
+UnSample(1:100, 4)
+UnSample(1:100, 5)
+UnSample(1:100, 9)
+UnSample(1:100, 10)
+UnSample(1:1000, 100)
